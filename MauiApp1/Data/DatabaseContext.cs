64903b9339e745619f604a1282a0fb261ef9c50e
Acsious <ulchenko.f@mail.ru>
2023-10-10 20:32:34 +0300

    Подготовка

---
+++ MauiApp1/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace MauiApp1.Data
+{
+    public class DatabaseContext
+    {
+        private const string DbName = "MyDatabase.db3";
+        private static string DbPath => Path.Combine(FileSystem.AppDataDirectory, DbName);
+
+        private SQLiteAsyncConnection _connection;
+        private SQLiteAsyncConnection Database =>
+            (_connection ??= new SQLiteAsyncConnection(DbPath,
+                SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache));
+
+    }
+}
