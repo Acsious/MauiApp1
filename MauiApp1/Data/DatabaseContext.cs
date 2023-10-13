f733b658662a2d40ec83ae4c5710f0323b25739a
Acsious <ulchenko.f@mail.ru>
2023-10-13 14:28:47 +0300

    Database Context complete

--- MauiApp1/Data/DatabaseContext.cs
+++ MauiApp1/Data/DatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using System.Linq.Expressions;
 
 namespace MauiApp1.Data
 {
@@ -12,5 +13,57 @@ namespace MauiApp1.Data
             (_connection ??= new SQLiteAsyncConnection(DbPath,
                 SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache));
 
+        private async Task CreateTableIfNotExist<TTable>() where TTable : class, new()
+        {
+            await Database.CreateTableAsync<TTable>();
+        }
+
+        private async Task<AsyncTableQuery<TTable>> GetTableAsync<TTable>() where TTable : class, new()
+        {
+            await CreateTableIfNotExist<TTable>();
+            return Database.Table<TTable>();
+        }
+
+        public async Task<IEnumerable<TTable>> GetAllAsync<TTable>() where TTable : class, new()
+        {
+            var table = await GetTableAsync<TTable>();
+            return await table.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TTable>> GetFileteredAsync<TTable>(Expression<Func<TTable, bool>> predicate) where TTable : class, new()
+        {
+            var table = await GetTableAsync<TTable>();
+            return await table.Where(predicate).ToListAsync();
+        }
+
+        public async Task<TTable> GetItemByKeyAsync<TTable>(object primaryKey) where TTable : class, new()
+        {
+            await CreateTableIfNotExist<TTable>();
+            return await Database.GetAsync<TTable>(primaryKey);
+        }
+
+        public async Task<bool> AddItemAsync<TTable>(TTable item) where TTable : class, new()
+        {
+            await CreateTableIfNotExist<TTable>();
+            return await Database.InsertAsync(item) > 0;
+        }
+
+        public async Task<bool> UpdateItemAsync<TTable>(TTable item) where TTable : class, new()
+        {
+            await CreateTableIfNotExist<TTable>();
+            return await Database.UpdateAsync(item) > 0;
+        }
+
+        public async Task<bool> DeleteItemAsync<TTable>(TTable item) where TTable : class, new()
+        {
+            await CreateTableIfNotExist<TTable>();
+            return await Database.DeleteAsync(item) > 0;
+        }
+
+        public async Task<bool> DeleteItemByKeyAsync<TTable>(object primaryKey) where TTable : class, new()
+        {
+            await CreateTableIfNotExist<TTable>();
+            return await Database.DeleteAsync<TTable>(primaryKey) > 0;
+        }
     }
 }
